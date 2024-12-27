dcb758096b2fab4e41f63ab7025170f794400485
Cvar1984 <cvar1984@cvar1984.my.id>
2024-12-27 12:02:51 +0700

    form register

--- web/add_whitelist.php
+++ web/add_whitelist.php
@@ -3,21 +3,137 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Cvar1984\Minecraft\MinecraftUser;
-// Example adding user to whitelist
-try {
-    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__, '/../.env'); // Point to the directory containing .env
-    $dotenv->load();
-    $whitelistPath = $_ENV['WHITELIST_PATH'];
-    $rconHost = $_ENV['RCON_HOST'];
-    $rconPort = (int)$_ENV['RCON_PORT'];
-    $rconPassword = $_ENV['RCON_PASS'];
-    $username = $argv[1] ?? 'Steve';
+use \Dotenv\Dotenv;
 
-    $minecraftUser = (new MinecraftUser($whitelistPath))->setupRcon($rconHost, $rconPort, $rconPassword);
-    $minecraftUser
-        ->addToWhitelist($username, MinecraftUser::ID_ONLINE_ONLY)
-        ->addToWhitelist($username, MinecraftUser::ID_OFFLINE_ONLY)
-        ->reloadWhitelist();
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+$whitelistPath = $_ENV['WHITELIST_PATH'];
+$rconHost = $_ENV['RCON_HOST'];
+$rconPort = (int)$_ENV['RCON_PORT'];
+$rconPassword = $_ENV['RCON_PASS'];
+$message = '';
+
+if (isset($_POST['username'])) {
+    try {
+        $username = $_POST['username'];
+        $minecraftUser = (new MinecraftUser($whitelistPath))->setupRcon($rconHost, $rconPort, $rconPassword);
+        $minecraftUser
+            ->addToWhitelist($username, MinecraftUser::ID_ONLINE_ONLY)
+            ->addToWhitelist($username, MinecraftUser::ID_OFFLINE_ONLY)
+            ->reloadWhitelist();
+            $message = 'Berhasil menambahkan ' . htmlspecialchars($username) . ' ke whitelist';
+    } catch (Exception $e) {
+        $message = $e->getMessage();
+    }
 }
+?>
+<!DOCTYPE html>
+<!--
+ ____________________________
+< Nothing to see here mooOoo >
+ ----------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://cvar1984.my.id/">
+    <title>VRChat Indonesia Minecraft Server</title>
+    <style>
+        @font-face {
+            font-family: bios;
+            src: url(https://cvar1984.github.io/fonts/ibm/3270-Regular.ttf);
+        }
+
+        :root {
+            height: 100%;
+            font-family: bios;
+        }
+
+        body {
+            display: flex;
+            margin: 0;
+            height: 100%;
+            background-color: #000;
+        }
+
+        main {
+            margin: auto;
+            /* margin: calc(50vh - 3rem) auto auto auto; */
+            text-align: center;
+        }
+
+        form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        input {
+            padding: 0.5rem;
+            margin: 0.5rem;
+            border-radius: 0.5rem;
+            border: 1px solid lime;
+            background-color: #112;
+            color: lime;
+        }
+
+        button {
+            padding: 0.5rem;
+            margin: 0.5rem;
+            border-radius: 0.5rem;
+            border: 1px solid lime;
+            background-color: #112;
+            color: lime;
+            cursor: pointer;
+        }
+
+        h1,
+        p {
+            margin: 0;
+            padding: 1rem 0;
+            font-size: 1.5rem;
+            color: lime;
+        }
+
+        pre {
+            margin: 1rem;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            background-color: #112;
+            color: #a99;
+            font-family: bios;
+        }
+
+        a {
+            text-decoration: none;
+            color: lime;
+        }
+    </style>
+    <?php if (!empty($message)) : ?>
+        <script>
+            alert("<?= addslashes($message); ?>");
+        </script>
+    <?php endif; ?>
+</head>
+
+<body>
+    <main>
+        <h1>Masukan Username Minecraft anda dibawah untuk daftar</h1>
+        <form method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <button type="submit">Daftar</button>
+        </form>
+    </main>
+</body>
+
+</html>
--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <!--
- ____________________________ 
+ ____________________________
 < Nothing to see here mooOoo >
- ---------------------------- 
+ ----------------------------
         \   ^__^
          \  (oo)\_______
             (__)\       )\/\
@@ -12,73 +12,73 @@
 -->
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="format-detection" content="telephone=no">
-	<meta name="robots" content="index,follow">
-	<link rel="canonical" href="https://cvar1984.my.id/">
-        <title>VRChat Indonesia Minecraft Server</title>
-        <style>
-            @font-face {
-                font-family: bios;
-                src: url(https://cvar1984.github.io/fonts/ibm/3270-Regular.ttf);
-            }
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://cvar1984.my.id/">
+    <title>VRChat Indonesia Minecraft Server</title>
+    <style>
+        @font-face {
+            font-family: bios;
+            src: url(https://cvar1984.github.io/fonts/ibm/3270-Regular.ttf);
+        }
 
-            :root {
-                height: 100%;
-                font-family: bios;
-            }
+        :root {
+            height: 100%;
+            font-family: bios;
+        }
 
-            body {
-                display: flex;
-                margin: 0;
-                height: 100%;
-                background-color: #000;
-            }
+        body {
+            display: flex;
+            margin: 0;
+            height: 100%;
+            background-color: #000;
+        }
 
-            main {
-                margin: auto;
-                /* margin: calc(50vh - 3rem) auto auto auto; */
-                text-align: center;
-            }
+        main {
+            margin: auto;
+            /* margin: calc(50vh - 3rem) auto auto auto; */
+            text-align: center;
+        }
 
-            h1,
-            p {
-                margin: 0;
-                padding: 1rem 0;
-                font-size: 1.5rem;
-                color: lime;
-            }
+        h1,
+        p {
+            margin: 0;
+            padding: 1rem 0;
+            font-size: 1.5rem;
+            color: lime;
+        }
 
-            pre {
-                margin: 1rem;
-                padding: 0.5rem;
-                border-radius: 0.5rem;
-                background-color: #112;
-                color: #a99;
-                font-family: bios;
-            }
+        pre {
+            margin: 1rem;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            background-color: #112;
+            color: #a99;
+            font-family: bios;
+        }
 
-            a {
-                text-decoration: none;
-                color: lime;
-            }
-        </style>
-    </head>
+        a {
+            text-decoration: none;
+            color: lime;
+        }
+    </style>
+</head>
 
-    <body>
-        <main>
-            <p>
-                Join minecraft server below<br />
-                <img height="100px" width="400px"
-                    src="https://api.mcstatus.io/v2/widget/java/cvar1984.my.id?dark=false&rounded=true" />
-            </p>
-            <pre>                      |              _) |         |                       
- __ \   _ \ __ `__ \  __ \   _ \  __| | __|  _` | __ \  |   |  _` | __ \  
- |   |  __/ |   |   | |   |  __/ |    | |   (   | | | | |   | (   | |   | 
- .__/ \___|_|  _|  _|_.__/ \___|_|   _|\__|\__,_|_| |_|\__,_|\__,_|_|  _| 
-_|                                                                        
+<body>
+    <main>
+        <p>
+            Join minecraft server below<br />
+            <img height="100px" width="400px"
+                src="https://api.mcstatus.io/v2/widget/java/cvar1984.my.id?dark=false&rounded=true" />
+        </p>
+        <pre>                      |              _) |         |
+ __ \   _ \ __ `__ \  __ \   _ \  __| | __|  _` | __ \  |   |  _` | __ \
+ |   |  __/ |   |   | |   |  __/ |    | |   (   | | | | |   | (   | |   |
+ .__/ \___|_|  _|  _|_.__/ \___|_|   _|\__|\__,_|_| |_|\__,_|\__,_|_|  _|
+_|
 
 -----BEGIN PGP SIGNED MESSAGE-----
 Hash: SHA512
@@ -106,16 +106,15 @@ OcUrAm2kp9ymxrQ7QLnFJMUwRRmvDPzz86rBfU9niDoeSkXWQw==
 =xOKb
 -----END PGP SIGNATURE-----
 </pre>
-<p>
-    [<a href="https://gitlab.com/Cvar1984/minecraft_java">Snapshoot Java</a>]
-    [<a href="https://gitlab.com/Cvar1984/minecraft_bedrock">Snapshoot Bedrock</a>]
-    [<a href="https://cvar1984.my.id/storage/minecraft/">Mods</a>]
-    [<a href="https://github.com/sponsors/Cvar1984">Sponsor</a>]<br/>
-    <a href="mailto:cvar1984@cvar1984.my.id">Client Support</a>
-</p>
-        </main>
-    </body>
+        <p>
+            [<a href="add_whitelist.php">Whitelist</a>]
+            [<a href="https://gitlab.com/Cvar1984/minecraft_java">Snapshoot Java</a>]
+            [<a href="https://gitlab.com/Cvar1984/minecraft_bedrock">Snapshoot Bedrock</a>]
+            [<a href="https://cvar1984.my.id/storage/minecraft/">Mods</a>]
+            [<a href="https://github.com/sponsors/Cvar1984">Sponsor</a>]<br />
+            [<a href="mailto:cvar1984@cvar1984.my.id">Client Support</a>]
+        </p>
+    </main>
+</body>
 
 </html>
-
-
